eddae63a88a9fad26a6ed98d59659a274fefc716
Khan Mohsin <m90khan@gmail.com>
2019-05-05 11:50:27 +0200

    typewrite resume

--- app.js
+++ app.js
@@ -6,26 +6,71 @@ const typewriter = new Typewriter(app, {
 
 });
 
-typewriter.typeString(`<h1>Hey There<h1>`)
+// const myName = Khan Mohsin
+// const workAs = Web Developer
+// let Seeking = Job
+// if (YOU === still interested){
+// // then take a look at my video resume below to know more about me.
+// Video Resume: https://www.khanweb.site/about.html
+// }else if (YOU === still not convinced){
+// // Please take a moment to review my enclosed resume and my personal website
+// Website: https://www.khanweb.site
+// }else{
+// // If you have any question, please send me an email.
+// Thank you for your time and consideration.
+// }
+const intro = 'Hi There'
+const myName = 'Khan Mohsin'
+const email = 'hello@khanweb.site';
+const workAs = 'Web Developer'
+let seek = 'Job'
+let you = 'your Company'
+let condition = ['still interested', 'still not convinced']
+const thankYou = 'Thank you for your time and consideration.'
+const hireMe = () =>{
+if ( you === condition[0]){
+console.log(` // then take a look at my video resume below to know more about me.`)
+const videoResume = 'https://www.khanweb.site/about.html'
+console.log(videoResume);
+}else if (you === condition[1]){
+console.log(`// Please take a moment to review my enclosed resume and my personal website`);
+const myWebsite= 'https://www.khanweb.site'
+console.log(myWebsite);
+} else {
+console.log(` // If you have any question, please send me an email at ${email}. \n${thankYou}`)
+}
+}
+typewriter.typeString(`<h1>${intro}<h1>`)
     .pauseFor(2000)
-    .typeString(  ` <h3>Lets start first with an little introduction about me<h3>`)
-    .typeString(`I come from Pakistan where i completed my college studies then
-                 I moved to China for my Bacheolrs`)
-                 .deleteChars(9)
-                 .typeString(`<strong>Bachelors</strong> in Electronics and Information Technology
-                   and now I am in Germany for my Masters`)
+    .typeString(  `// Lets start first with an little intrdoctoin`)
+    .deleteChars(7)
+    .typeString(`<strong>oduction</strong> about me`)
+    .typeString(  ` <h3>const myName = '${myName}'<h3>`)
+    .typeString(  ` <h3>const email = '${email}'<h3>`)
+    .typeString(  ` <h3>const workAs = '${workAs}'<h3>`)
+    .typeString(  ` <h3>let seek = '${seek}'<h3>`)
+    .typeString(  ` <h3>let condition = [${condition}]<h3>`)
+    .typeString(  ` <h3>let you = ${you}</h3>`)
+    .typeString(  ` <h3>const hireMe = () =>{ <h3>`)
+    .typeString(  ` <h3>if ( you === '${condition[0]}'){<h3>`)
+    .typeString(  ` // then take a look at my video resume below to know more about me.`)
+    .typeString(  ` <h3>const videoResume = 'https://www.khanweb.site/about.html'<h3>`)
+    .typeString(  ` <h3>}else if (you === '${condition[0]}'){<h3>`)
+    .typeString(`// Please take a moment to review my resume and my personal wbeiste`)
+                 .deleteChars(7)
+                 .typeString(`<strong>website</strong>`)
     .pauseFor(2500)
-    .typeString(` <h3>I am passionate about web development especially Front-End. I like the interaction while building things</h3> ` )
-    .typeString(` <h3>Some of the skills are: </h3>` )
-    .typeString(` <li>HTML5, CSS3 (Sass), Bootstrap JavaScript, React, jQuery, PHP, MySQL, Ajax, JSON, APIs</li>` )
-    .typeString(` <li>Node.js, npm, webpack, polyfill, Babel, Git, CLI</li>` )
-    .typeString(` <li>Convert HTML/CSS to WordPress Themes and understanding of Drupal</li>` )
-    .typeString(` <li>Web Design, User Experience, Wireframing, Prototyping, Branding & Identity, Graphic Design</li>` )
-    .typeString(` <li>UX tests to test the usability websites</li>` )
-    .typeString(` <li>Analyze traffic and make necessary changes for a better user experience.</li>` )
-    .typeString(` <li>Photoshop, Illustrator, XD, Afteraffects, Balsamiq, Axure RP, niice, fullstory, invision, Trello.</li>` )
-    .typeString(` <li>I can speak English, Urdu, Punjabi, Deutsch and Mandarin.</li>` )
+    .typeString(` <h3>const myWebsite= 'https://www.khanweb.site'</h3> ` )
+    .typeString(` <h3>} else {</h3>` )
+    .typeString(` // If you have any question, please send me an email at ${email}.` )
+    .typeString(` <h3>${thankYou}</h3>` )
+    .typeString(` <h3>}</h3>` )
+    .typeString(` <h3>}</h3>` )
     .pauseFor(2500)
-    .typeString(` <h2>Shoot me a message to start this journey together</h2>` )
+    .typeString(` <h3>hireme();</h3>` )
+    .typeString(` <h2>What? You don't think my code works</h2>` )
+    .typeString(` <h2>Look into the console</h2>` )
+    .start();
 
-      .start();
+
+    console.log(`${intro}\nMy name is ${myName}.\nMy email is ${email}.\nI am a ${workAs} and currently\nlooking for a ${seek}\nin ${you}\nso hireMe=${hireMe}`)
